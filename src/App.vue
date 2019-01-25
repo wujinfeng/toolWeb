edc3b57d1264edbf5107e282b7907a9989e0c2e3
吴金锋 <wujinfeng@iyunxiao.com>
2019-01-25 21:03:59 +0800

    add:init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Md5 msg="md5加密"/>
+  </div>
+</template>
+
+<script>
+import Md5 from './components/Md5.vue'
+
+export default {
+  name: 'app',
+  components: {
+      Md5
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Md5.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Md5',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
